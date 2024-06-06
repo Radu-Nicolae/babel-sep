663786be957add945f7853f3e63bc8aa56d0ee4f
stefan911 <danielstefan.halasz@gmail.com>
2024-06-06 16:25:06 +0200

    fix: We can now use ranges of identifiers in order to test coverage on a specific range of functions.
    This is so that we can check coverage on a specific interval, such that we can rule out a specific function or file.

--- babel/sep_coverage.py
+++ babel/sep_coverage.py
@@ -4,6 +4,8 @@ code_set: set = set()
 Instrument a line of code with a unique ID =
 Register the ID in a set, marking that line 
 of code as reached during execution.
+
+@param identifier: A unique ID for the line of code.
 """
 
 
@@ -15,15 +17,15 @@ def instrument(identifier: int) -> None:
 Create a list of all instrumented lines of code that are never
 reached during execution.
 
-@param max_instrument: The maximum instrumented line ID for the code.
+@param range_ids: A tuple containing the first and last instrumented line IDs.
 @return 
 """
 
 
-def get_coverage(max_instrument_id: int) -> list[int]:
-    sorted_ids = sorted(code_set)
-    unaccessed_line_ids : list = []
-    last_id = -1
+def get_coverage(range_ids: tuple[int, int]) -> list[int]:
+    sorted_ids: list = sorted(code_set)
+    unaccessed_line_ids: list = []
+    last_id: int = range_ids[0] - 1
 
     for instrument_id in sorted_ids:
         # If no ID was skipped
@@ -36,7 +38,7 @@ def get_coverage(max_instrument_id: int) -> list[int]:
         last_id = instrument_id
 
     # Add all skipped ids until the last instrumented line
-    for i in range(last_id + 1, max_instrument_id + 1):
+    for i in range(last_id + 1, range_ids[1] + 1):
         unaccessed_line_ids.append(i)
 
     return unaccessed_line_ids
@@ -44,12 +46,12 @@ def get_coverage(max_instrument_id: int) -> list[int]:
 
 """
 Computes the coverage percentage of the instrumented code.
-@param max_instrument: The maximum instrumented line ID for the code.
+@param range_ids: A tuple containing the first and last instrumented line IDs.
 @return: The coverage percentage as a floating point [0.0, 100.0].
 """
 
 
-def coverage_percentage(max_instrument):
-    total_lines_of_code = max_instrument + 1
-    percentage = (1 - len(get_coverage(max_instrument)) / total_lines_of_code) * 100
+def coverage_percentage(range_ids: tuple[int, int]) -> float:
+    total_lines_of_code = range_ids[1] - range_ids[0] + 1
+    percentage = (1 - len(get_coverage(range_ids)) / total_lines_of_code) * 100
     return round(percentage, 2)
--- init_sep_coverage.py
+++ init_sep_coverage.py
@@ -2,10 +2,18 @@ import babel.sep_coverage as sep
 import pytest
 
 
-def main():
+def test_coverage_A():
+    LOWER_RANGE = 0
+    UPPER_RANGE = 2
+    range_ids = (LOWER_RANGE, UPPER_RANGE)
+
     pytest.main()
-    print(sep.get_coverage(2))
-    print(f"Coverage percentage: {sep.coverage_percentage(2)}%")
+    print(sep.get_coverage(range_ids))
+    print(f"Coverage percentage: {sep.coverage_percentage(range_ids)}%")
+
+
+def main():
+    test_coverage_A()
 
 
 if __name__ == "__main__":
