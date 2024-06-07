772f5f708cef3e6015d32597c81459f4e3ea3ae9
Radu Nicolae <rnicolae04@gmail.com>
2024-06-07 20:15:00 +0300

    added test_radu.py

---
+++ README.md
@@ -0,0 +1,7 @@
+# README.md
+
+GITBOOK - https://sep-vu.gitbook.io/software-engineering-processes/assignment-1-testing
+
+The group's work should be reported as the README.md file in the root of the forked repository (the original README.md 
+file should be renamed to README_original.md). The README.md file should follow the report template for this assignment 
+(see Section 2).
---
+++ tests/test_radu.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+import babel.dates as dates
+
+def test_format_date():
+    # put 0d like 07
+    assert dates.format_date(locale="en") == datetime.now().strftime("%b %-d, %Y")
+
+
+def test_datetimepattern_str():
+    date_time_pattern = dates.DateTimePattern(pattern="yyyy-MM-dd", format="long")
+    assert str(date_time_pattern) == "yyyy-MM-dd"
+
